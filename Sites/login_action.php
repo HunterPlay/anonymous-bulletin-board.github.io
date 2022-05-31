50034bac783354a0521906fcfc07dd4fdaca25d3
HunterPlay <sa2000808@gmail.com>
2022-05-31 23:57:52 +0900

    first commit

---
+++ Sites/login_action.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+
+$user_id = $_POST['user_id'];
+$password = $_POST['password'];
+
+if($user_id == '123' && $password == '123') {
+    $_SESSION['user_id'] = $user_id;
+} else {
+echo "ログインID もしくは パスワードが間違っています。";
+}
+
+if(isset($_SESSION['user_id'])) {
+    header('Location: index.php');
+    exit;
+}
